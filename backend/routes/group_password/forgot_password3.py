f622265bf395c821cd503c11459bf511b4bbd4ff
laivansam <samvasang1192011Ggmail.com>
2026-01-25 18:04:29 +0700

    fix: lỗi import ngu

--- backend/routes/group_password/forgot_password3.py
+++ backend/routes/group_password/forgot_password3.py
@@ -1,9 +1,9 @@
-from flask import Blueprints
+from flask import Blueprint
 #import nội bộ
 from middleware.rate_limiting import limit_requests
 #from controllers.group_password.forgot_password3 import ...
 
-app_route5 = Blueprints("auth_xac_nhan_token", __name__)
+app_route5 = Blueprint("auth_xac_nhan_token", __name__)
 @app_route5.route("/tim-mat-khau3", methods=["POST"])
 
 @limit_requests(max_requests=5, period=60)
